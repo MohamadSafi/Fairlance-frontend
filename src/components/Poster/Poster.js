85982733db681d965871474fe95f70cc65564c47
MohamadSafi <the.mohammad.safi@gmail.com>
2023-10-07 02:09:29 +0300

    Adding motion to Main View

--- src/components/Poster/Poster.js
+++ src/components/Poster/Poster.js
@@ -1,16 +1,25 @@
 import React from 'react';
 import { StyledPoster } from './style';
+import { motion } from 'framer-motion';
 
 const Poster = ({ img, left, Content }) => {
   return (
     <StyledPoster className='main-view'>
       {left ? (
-        <img src={img} alt='' draggable='false' style={{ marginRight: 'auto', marginLeft: 0 }} />
+        <motion.div animate={{ x: 100 }} initial={{ x: 400 }} transition={{ delay: 0.1 }}>
+          <img src={img} alt='' draggable='false' style={{ marginRight: 'auto', marginLeft: 0 }} />
+        </motion.div>
       ) : (
         ''
       )}
       <Content />
-      {!left ? <img src={img} alt='' draggable='false' /> : ''}
+      {!left ? (
+        <motion.div animate={{ x: 100 }} initial={{ x: 400 }} transition={{ delay: 0.1 }}>
+          <img src={img} alt='' draggable='false' />{' '}
+        </motion.div>
+      ) : (
+        ''
+      )}
     </StyledPoster>
   );
 };
